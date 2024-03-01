4c8d9c62f7baf5e7ab25ace115f485e673fa98c8
i-pereyaslovskiy <i.pereyaslovskiy@gmail.com>
2024-03-01 14:00:54 +0300

    Добавьте файлы проекта.

---
+++ F.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otus_Reflection
+{
+    internal class F
+    {
+        int i1, i2, i3, i4, i5;
+        public F Get() => new F() { i1 = 1, i2 = 2, i3 = 3, i4 = 4, i5 = 5 };
+    }
+}
